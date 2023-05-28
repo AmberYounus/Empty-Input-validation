1079a5a2b55b436653ab8783d66074eddf17a7ea
AmberYounus <65040261+AmberYounus@users.noreply.github.com>
2023-05-28 13:31:58 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function validateForm() {
+  var form = document.forms["myForm"]["fname"].value;
+  if (form == "" || form == null) {
+    alert("Name must be filled out");
+    return false;
+  }
+}
